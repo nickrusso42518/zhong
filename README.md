a5c69ff254fe33b1e5836aa0b5f70b76c2e04ba1
Nicholas Russo <njrusmc@gmail.com>
2022-07-22 09:32:23 -0400

    verbal

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chinese Language Trainer
+Work in progress
--- play.py
+++ play.py
@@ -1,8 +1,12 @@
 import json
 import random
+import subprocess
 
 from colorama import Fore
 from colorama import Style
+
+# Toggle reading of Chinese symbols
+SPEAK = True
 
 with open("symbols.json", encoding="utf=8") as handle:
     symbols = json.load(handle)
@@ -10,20 +14,26 @@ with open("symbols.json", encoding="utf=8") as handle:
 # jsonschema here
 
 print("NOTE: use numeric pinyin format: zhong1, cha2, hao3, jian4, ma\n")
+count = 1
 while symbols:
-    count = 1
     index = random.randint(0, len(symbols) - 1)
     sym = symbols[index]
     pinyin = sym["pinyin"].lower().strip()
     english = sym["english"].lower().strip()
 
-    attempt = input(f"{count}. Enter pinyin,english for {sym['chinese']}: ")
+    attempt = ""
+    while not attempt.strip():
+        print(f"\n{count}. Symbols (press ENTER to reprint): {sym['chinese']}")
+        if SPEAK:
+            subprocess.run(["say", "-v", "Ting-Ting", sym["chinese"]])
+        attempt = input(f"Type the pinyin,english: ")
+
     in1, in2 = attempt.split(",")
 
     p_color = Fore.GREEN if in1.lower().strip() == pinyin else Fore.RED
     e_color = Fore.GREEN if in2.lower().strip() == english else Fore.RED
     print(f"pinyin: {p_color}{pinyin}{Style.RESET_ALL}", end="")
-    print(f"  /  english: {e_color}{english}{Style.RESET_ALL}\n")
+    print(f"  /  english: {e_color}{english}{Style.RESET_ALL}")
 
     del symbols[index]
     count += 1
